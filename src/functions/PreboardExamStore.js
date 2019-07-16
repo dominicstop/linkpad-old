c0da1902f4026d634308a746636c39b29ff56cd2
Dominic Go <dominicstop@gmail.com>
2019-07-17 07:34:16 +0800

    preboardexam wip

--- src/functions/PreboardExamStore.js
+++ src/functions/PreboardExamStore.js
@@ -1,219 +1,243 @@
 import store from 'react-native-simple-store';
 import _ from 'lodash';
-//import Moment from 'react-moment';
+import { fetchWithProgress } from './Utils';
 
-const DEBUG = true;
-const URL = 'https://linkpad-pharmacy-reviewer.firebaseapp.com/getallpreboardexams';
-const KEY = 'preboard';
 
 let _preboardData = null;
 
-//model: structure of single exammodule item in preboard exams.exammodule array
-export class PreboardExamModuleItem {  
-  constructor(examModule = { description: '', indexid: 0, premodulename: '', questions: []},  indexid_exam = null){
-    this.examModule = {
-      //used for identifying which exam the exammodule belongs to
-      indexid_exam,
-      ...examModule,
-    };
-  }
+export class PreboardExamChoice {
+  static structure = {
+    value: '',
+    //added afer processing
+    choiceID         : -1   , //unique id for use in lists and comparison (in case the choices are falsy i.e null/undef etc.) or have the same exact string
+    isAnswer         : false, //is the correct answer (choices and answers are merged together, or in cases where there are mult. correct answers)
+    indexid_question : -1   , //passed down from question - which question  this choice belongs to
+    indexid_premodule: -1   , //passed down from question - which premodule this choice belongs to
+    questionID       : ''   , //passed down from question (used for reconciling when they are separated)
+  };
 
-  get = () => {
-    return _.cloneDeep(this.examModule);
-  }
+  static wrap(){
 
-  getQuestionCount = () => {
-    const { examModule } = this;
-    return examModule && examModule.questions.length;
-  }
+  };
 
-  getCompositeIndexid = () => {
-    const { indexid_exam, indexid} = this.examModule;
-    return `examid:${indexid_exam}-moduleid:${indexid}`;
-  }
-}
+  static wrapArray(){
 
-//model: structure of single exam item in preboard exams array
+  };
+};
+
+export class PreboardExamAnswer {
+  static structure = {
+    answer   : PreboardExamChoice.structure, //the selected answer
+    isCorrect: false, //true when matches the correct answer
+    timestamp: -1   , //timestamp of when the answer was made
+    answerID : -1   , //derived from choiceID
+  };
+
+  static wrap(){
+
+  };
+
+  static wrapArray(){
+
+  };
+
+  static create(){
+
+  };
+};
+
+export class PreboardExamQuestion {
+  static structure = {
+    answer     : '',
+    choices    : [],
+    explanation: '',
+    question   : '',
+    //added after processing
+    indexid_premodule: -1, //which premodule this question belongs to
+    questionID       : '', //unique id for use in lists and comparison
+  };
+
+  static wrap(){
+
+  };
+
+  static wrapArray(){
+
+  };
+};
+
+export class PreboardExamModule {
+  static structure = {
+    indexid      : -1, 
+    premodulename: '',
+    description  : '',
+    questions    : [],
+  };
+
+  static wrap(){
+
+  };
+
+  static wrapArray(){
+
+  };
+};
+
 export class PreboardExamItem {
-  constructor(exam = {dateposted: '', description: '', enddate: '', exammodules: [], examname: '', indexid: -1, startdate: ''}){
-    this.exam = exam;
-  }
-
-  get = () => {
-    return _.cloneDeep(this.exam);
+  static structure = {
+    indexid    : '',
+    examname   : '',
+    description: '',
+    dateposted : '',
+    startdate  : '',
+    enddate    : '',
+    timelimit  : -1,
+    exammodules: [],
   };
 
-  getExamModules = () => {
-    const { exam } = this;
-    //note: written like this for VSCODE autocomplete to work
-    let exams = [new PreboardExamModuleItem()];
-    exams = exam.exammodules.map((item) => 
-      //wrap inside a model, but return as object
-      new PreboardExamModuleItem(item, exam.indexid).examModule
-    );
-    return exams;
+  static wrap(){
+
   };
 
-  getExamModulesModel = () => {
-    //note: written like this for VSCODE autocomplete to work
-    let exams = [new PreboardExamModuleItem()];
-    exams = this.getExamModules().map((item) => new PreboardExamModuleItem(item));
-    return exams;
+  static wrapArray(){
+
   };
+};
 
-  getTotalModules = () => {
-    return this.exam.exammodules.length;
-  }
-
-  getTotalQuestions = () => {
-    const modules = this.getExamModules();
-    let questionsCount = 0;
-    modules.forEach(item => {
-      let module =  new PreboardExamModuleItem(item);
-      questionsCount += module.examModule.questions.length;
-    });
-    return questionsCount;
-  };
-}
-
-//model: represents the structure of the json response for exam
 export class PreboardExam {
-  constructor(response = {message: '', success: false, exams: [], active: false, examkey: 0}){
-    this.response = response;
-  }
+  static structure = {
+    message: ''   ,
+    success: false,
+    active : false,
+    examkey: -1   , 
+  };
+};
 
-  //returns the json
-  get = () => {
-    return _.cloneDeep(this.response);
-  }
 
-  isActive = () => this.response.active;
 
-  //returns an array of PreboardExamItem
-  getExams = () => {
-    const { response } = this;
-    //note: written like this for VSCODE autocomplete to work
-    let exams = [new PreboardExamItem(response.exams[0])];
-    //could have used foreach but needed to skip index 0
-    for (let i = 1; i < response.exams.length; i++) {
-      exams.push(new PreboardExamItem(response.exams[i]));
-    }
-    return exams;
-  }
-
-  getActiveExam = () => {
-    const { exams, examkey } = this.get();
-    return exams[examkey];
-  }
-  
-  //active exam wrapped inside of PreboardExamItem model
-  getActiveExamAsModel = () => {
-    const { exams, examkey } = this.get();
-    return new PreboardExamItem(exams[examkey]);
-  }
-}
-
-export class PreboardExamManager {
-  constructor(){
-    this.data = _preboardData;
-  }
-
-  refresh = async () => {
-    this.data = await refresh();
+export class PreboardExamstore {
+  static get KEY(){
+    return 'preboard';
   };
 
-  //return preboard wrapped in model
-  getAsModel = async () => {
-    const data = await get();
-    this.data = data;
-    let model = new PreboardExam(data);
-    return model;
-  }
+  /** describes the current operation being done*/
+  static STATUS = {
+    READING      : 'READING',
+    WRITING      : 'WRITING',
+    FETCHING     : 'FETCHING',
+    SAVING_IMAGES: 'SAVING_IMAGES',
+    FINISHED     : 'FINISHED',
+  };
 
-  getActiveExamModel = async () => {
-    //get Preboard data as model
-    let preboardModel = await this.getAsModel();
-    //get active exam
-    return preboardModel.getActiveExamAsModel();
-  }
-}
+  static get URL(){
+    return 'https://linkpad-pharmacy-reviewer.firebaseapp.com/getallpreboardexams';
+  };
 
-fetchPreboard = () => {
-  return new Promise(async (resolve, reject) => {
+  static async fetch(){
     try {
-      let results = await fetch(URL);
+      //get preboard from server
+      let results = await fetch(PreboardExamstore.URL);
       let json    = await results.json();
-      resolve(json);
-      
+      //resolve
+      return (json);
+
     } catch(error) {
-      reject('Preboard fetchPreboard error.');
-    }
-  });
-}
+      console.log('Failed to fetch preboard from server.');
+      console.log(error);
+      throw error;
+    };
+  };
 
-get = () => {
-  return new Promise(async (resolve, reject) => {
+  static async fetchAndSaveWithProgress(callback){
     try {
-      //has not been set, init with storage
-      if(_preboardData == null){
-        //get modules from storage
-        if (DEBUG) console.log('\nReading preboard from storage...');
-        _preboardData = await store.get(KEY);
-      }
-      //if stil empty after init with value from stotage
-      if(_preboardData == null){
-        //fetchPreboard from server
-        if (DEBUG) console.log('Nothing stored. Getting preboard from server...');
-        let new_preboard = await fetchPreboard();
-        //assign to global variable
-        _preboardData = new_preboard;
-        //write preboard to storage
-        if (DEBUG) console.log('Writting preboard to storage...');
-        await store.save(KEY, _preboardData);      
-      }
-      //resolve preboard data
-      resolve(_preboardData);
-  
+      const { STATUS } = PreboardExamstore;
+      const progressCallback = (loaded) => {
+        //subtract 5% from the total percentage
+        const percent = Math.round((loaded/105) * 100);
+        callback && callback(percent, STATUS.FETCHING);
+      };
+
+      //fetch data from server
+      const response = await fetchWithProgress(PreboardExamstore.URL, progressCallback);
+      const json = response.data;
+
+      //makes sure all of the properties exists and has a default value
+      //todo: implement filter
+
+      callback && callback(95, STATUS.SAVING_IMAGES);
+      //process base64 images and store      
+      //const preboard = await _saveBase64ToStorage(json);
+      
+      callback && callback(97, STATUS.WRITING);
+      //write to storage
+      await store.save(PreboardExamstore.KEY, json);
+
+      callback && callback(100, STATUS.FINISHED);    
+      return json;
+
     } catch(error){
-      //some error occured
-      if (DEBUG) console.log('preboard Error: ' + error);
-      reject(error);
-    }
-  })
-}
+      console.log('fetchAndSaveWithProgress: unable to save/fetch preboard');
+      console.log(error);
+      throw error;
+    };
+  };
 
-refresh = async () => {
-  if (DEBUG) console.log('\nRefreshing preboard...');
-  try {
-    //fetchPreboard modules from server
-    if (DEBUG) console.log('fetchPreboarding preboard from server...');
-    let new_preboard = await fetchPreboard();
+  /** read preboard from store */
+  static async read(){
+    try {
+      //read from store
+      const data = await store.get(PreboardExamstore.KEY);
 
-    //delete previous preboard stored
-    if (DEBUG) console.log('Deleting previous stored preboard...');
-    await store.delete(KEY);
+      //update cache var
+      _preboardData = data;
+      //resolve
+      return (data);
 
-    //update global var
-    _preboardData = new_preboard;
+    } catch(error){
+      console.log('Failed to read preboard from store.');
+      console.log(error);
+      throw error;
+    };
+  };
 
-    //write to storage
-    if (DEBUG) console.log('Writing preboard to storage');
-    await store.save(KEY, _preboardData);
+  static async refresh(status){
+    const { STATUS } = PreboardExamstore;
 
-  } catch(error) {
-    //some error occured
-    if (DEBUG) console.log('Refresh preboard Failed: ' + error);
-    return Promise.reject(new Error('Preboard refresh error.'));
-  }
-  //resolve preboard data
-  return(_preboardData);
-}
+    try {
+      status && status(STATUS.FETCHING);
+      //fetch preboard from server
+      const new_preboard = await PreboardExamstore.fetch();
 
-clear = () => _preboardData = null;
+      //check for changes
+      const ispreboardNew = !_.isEqual(_preboardData, new_preboard);
 
-export default {
-  fetchPreboard,
-  get,
-  refresh,
-  clear,
-}
+      status && status(STATUS.WRITING);      
+      //delete previous preboard stored
+      await PreboardExamstore.delete();
+      //write preboard to storage
+      await store.save(PreboardExamstore.KEY, preboard_wrapped);
+
+      //update cache var
+      _preboardData = new_preboard;
+
+      status && status(STATUS.FINISHED);
+      //resolve
+      return ({preboard: new_preboard, ispreboardNew});
+
+    } catch(error) {
+      console.log('Unable to refresh preboard.');
+      console.log(error);
+      
+      throw error;
+    };
+  };
+
+  static clear(){
+    _preboardData = null;
+  };
+
+  static async delete(){
+    _preboardData = null;
+    await store.delete(PreboardExamstore.KEY);
+  };
+};
